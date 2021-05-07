dd4353cb39cc0b22339e56cea2a2d4c496e9b74c
Jihane-Raoui <67758911+Jihane-Raoui@users.noreply.github.com>
2021-05-07 07:17:07 +0000

    Update HeaderComponent.jsx

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -17,9 +17,10 @@ class HeaderComponent extends Component {
                 <header>
                     <nav className="nav navbar navbar-expand-lg navbar-dark bg-dark" >
                               <div ><a href="/login" className='navbar-brand' >  User management</a></div>
-                              <div ><a href="/dashboard" className='navbar-brand' > Dashboard</a></div>
+                              
                        <ul className="navbar-nav navbar-collapse justify-content-end">
                        <li><a className="nav-link " href="/logout" onClick={AuthenticationService.logout}>Logout</a></li>
+                             <li><a  className="nav-link " href="/dashboard" className='navbar-brand' > Dashboard</a></li>
                               </ul>
                     </nav>
                 </header>
